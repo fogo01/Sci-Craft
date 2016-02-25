6fc022222d2110bc6686b067eab5180634dbac5d
fogo01 <axel.hedvall@gmail.com>
2016-02-25 16:30:11 +0100

    Moon stuff
    Added sky renderer for the moon

--- src/main/java/com/fogo01/scicraft/dimensions/WorldProviderMoon.java
+++ src/main/java/com/fogo01/scicraft/dimensions/WorldProviderMoon.java
@@ -2,9 +2,17 @@ package com.fogo01.scicraft.dimensions;
 
 import com.fogo01.scicraft.init.BiomeRegistry;
 import com.fogo01.scicraft.reference.Dimensions;
+import com.fogo01.scicraft.render.dimensions.CloudRenderer;
+import com.fogo01.scicraft.render.dimensions.SkyRenderer;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.Vec3;
+import net.minecraft.world.World;
 import net.minecraft.world.WorldProvider;
 import net.minecraft.world.biome.WorldChunkManagerHell;
 import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraftforge.client.IRenderHandler;
 
 public class WorldProviderMoon extends WorldProvider {
     @Override
@@ -14,8 +22,7 @@ public class WorldProviderMoon extends WorldProvider {
     }
 
     public IChunkProvider createChunkGeneration() {
-        return null;
-        //return new ChunkProviderMoon(worldObj, worldObj.getSeed(), true);
+        return new ChunkProviderMoon(worldObj, worldObj.getSeed(), true);
     }
 
     @Override
@@ -26,5 +33,89 @@ public class WorldProviderMoon extends WorldProvider {
     @Override
     public boolean canRespawnHere() {
         return false;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public boolean renderStars() {
+        return true;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public double getMovementFactor() {
+        return 0.1;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public float getStarBrightness(World world, float f) {
+        return 1.0F;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public boolean renderClouds() {
+        return true;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public boolean renderVoidFog() {
+        return false;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public float setSunSize() {
+        return 0.25F;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public float setMoonSize() {
+        return 4.0F;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public Vec3 getSkyColor(Entity cameraEntity, float partialTicks) {
+        return worldObj.getSkyColorBody(cameraEntity, partialTicks);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public boolean isSkyColored() {
+        return true;
+    }
+
+    @Override
+    public boolean isSurfaceWorld() {
+        return true;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public float getCloudHeight() {
+        return this.terrainType.getCloudHeight();
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public String getWelcomeMessage() {
+        return "Landing on the Moon";
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public String getDepartMessage() {
+        return "Leaving the Moon";
+    }
+
+    @Override
+    public IRenderHandler getSkyRenderer() {
+        return new SkyRenderer();
+    }
+
+    @Override
+    public IRenderHandler getCloudRenderer() {
+        return new CloudRenderer();
+    }
+
+    @Override
+    public Vec3 drawClouds(float partialTicks) {
+        return super.drawClouds(partialTicks);
     }
 }
